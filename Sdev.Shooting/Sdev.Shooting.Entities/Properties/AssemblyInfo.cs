d1136ace195c6f1c917c4c021370f9eee0a56400
StephanAndert <stephanandert@hotmail.de>
2012-09-19 19:17:19 +0200

    Die Entities für den Bereich Shooting wurden hinzugefügt.

---
+++ Sdev.Shooting/Sdev.Shooting.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sdev.Shooting.Entities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sdev.Shooting.Entities")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d1b7c68c-a302-405c-82fe-eee3cffecda3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sdev.Shooting/Sdev.Shooting.Entities/Shooting/Entities/Shot.cs
@@ -0,0 +1,17 @@
+﻿namespace Sdev.Shooting.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Klasse für einen Schuß.
+    /// </summary>
+    public class Shot
+    {
+        #region Value
+        /// <summary>
+        /// Liefert oder setzt den Value.
+        /// </summary>
+        public double Value { get; set; }
+        #endregion
+    }
+}
