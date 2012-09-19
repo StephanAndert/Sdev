e8b478853ab86784c8c03bcb02efe2b8f2240fe4
StephanAndert <stephanandert@hotmail.de>
2012-09-19 19:11:23 +0200

    Erstes Projekt zu GitHub hinzugefügt.

---
+++ Sdev.Shooting/Sdev.Shooting/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sdev.Shooting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sdev.Shooting")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fe8e930c-5d2a-406d-83b2-d6eb70618c11")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
