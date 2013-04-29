87fd1b9d33284606071cfffdf99d20919ad25e54
StephanAndert <stephanandert@hotmail.de>
2013-04-29 21:03:17 +0200

    Entitäten für die Transaction hinzugefügt. Mit dieser Klasse können Einnahmen sowie Ausgaben verwaltet werden.

---
+++ Sdev.BudgetManager/Sdev.BudgetManager.Entities/BudgetManager/Entities/Transaction.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Transaction.cs" company="Stephan Dev">
+//     Copyright (c) Stephan Dev. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Sdev.BudgetManager.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Class for a transaction. A Transaction is an income or an outcome.
+    /// </summary>
+    public class Transaction
+    {
+        /// <summary>
+        /// Gets or sets the transactionId.
+        /// </summary>
+        public Guid TransactionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the transaction.
+        /// </summary>
+        public TransactionType TransactionType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the amount.
+        /// </summary>
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Sdev.BudgetManager/Sdev.BudgetManager.Entities/BudgetManager/Entities/TransactionType.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TransactionType.cs" company="Stephan Dev">
+//     Copyright (c) Stephan Dev. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Sdev.BudgetManager.Entities
+{
+    /// <summary>
+    /// Class for the possible types of a transaction.
+    /// </summary>
+    public enum TransactionType
+    {
+        /// <summary>
+        /// The transaction is an income.
+        /// </summary>
+        Income,
+
+        /// <summary>
+        /// the transaction is an outcome.
+        /// </summary>
+        Outcome,
+    }
+}
