33cc01f577e3f15bb6895fb3e1bbad51878907c9
StephanAndert <stephanandert@hotmail.de>
2013-04-29 20:48:40 +0200

    Project für die Entities zur Quellcodeverwaltung hinzugefügt.

---
+++ Sdev.BudgetManager/Sdev.BudgetManager.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Stephan Dev">
+//     Copyright (c) Stephan Dev. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sdev.BudgetManager.Entities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sdev.BudgetManager.Entities")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a8887973-7294-4d42-bdcc-e029158007e9")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
