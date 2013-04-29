7aadcd4b507fc03f110cbff37c62223487a10eb3
StephanAndert <stephanandert@hotmail.de>
2013-04-29 20:44:48 +0200

    Solution für den BudgetManager zur Quellcodeverwaltung hinzugefügt.

---
+++ Sdev.BudgetManager/Sdev.BudgetManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Stephan Dev">
+//     Copyright (c) Stephan Dev. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sdev.BudgetManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sdev.BudgetManager")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a5d406c9-83f3-4a20-9ff2-2b1a168475c5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
